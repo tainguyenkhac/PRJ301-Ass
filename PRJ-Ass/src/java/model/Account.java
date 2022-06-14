eb0b25eedebc07b95a0ed2d66db95c0bd415cc1d
tainguyenkhac <99312851+tainguyenkhac@users.noreply.github.com>
2022-06-14 09:50:14 +0700

    login

---
+++ PRJ-Ass/src/java/model/Account.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Asus
+ */
+public class Account {
+    private String username;
+    private String pass;
+    private String role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole (String role) {
+        this.role = role;
+    }
+}
---
+++ PRJ-Ass/web/js/loginjs.js
@@ -0,0 +1,16 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/gulpfile.js to edit this template
+ */
+
+$(".user").focusin(function(){
+  $(".inputUserIcon").css("color", "#e74c3c");
+}).focusout(function(){
+  $(".inputUserIcon").css("color", "white");
+});
+
+$(".pass").focusin(function(){
+  $(".inputPassIcon").css("color", "#e74c3c");
+}).focusout(function(){
+  $(".inputPassIcon").css("color", "white");
+});
